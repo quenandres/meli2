bfec026fc9f49ef9496fb286636e6c62cd12b920
Jhonatan Mejia <quenandres@gmail.com>
2022-07-14 18:04:52 -0500

    Implementa cache en la ruta principal de la petición

--- api/src/index.js
+++ api/src/index.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const app = express();
 const morgan=require('morgan');
-const router = express.Router();
+const responseTime = require("response-time");
+const redis = require("redis");
+const { promisify } = require("util");
 const axios = require('axios');
+
+const client = redis.createClient({  
+  host: '127.0.0.1',
+  port: 6379,
+  legacyMode: true
+});
+
+const GET_ASYNC = promisify(client.get).bind(client);
+const SET_ASYNC = promisify(client.set).bind(client);
+
+client.connect();
+client.on('error', (err) => console.log('Redis Client Error', err));
 
 //Configuraciones
 app.set('port', process.env.PORT || 6060);
 app.set('json spaces', 2)
+// Añade middleware para ver las cabeceras de tiempo en las solicitudes
+app.use(responseTime());
  
 //Middleware
 app.use(morgan('dev'));
@@ -14,7 +30,6 @@ app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 const cors      = require('cors');
 
-app.use('/api/v1', router);
 
 
 const whitelist = ['http://localhost:5500', 'http://localhost:3000', 'http://127.0.0.1:5500','http://127.0.0.1:5500/nodejs/backend-api-rest/index.html'];
@@ -30,38 +45,40 @@ const optionsCors = {
 
 app.use(cors(optionsCors));
 
-
-//Nuestro primer WS Get
 const __API__ = 'https://api.spaceflightnewsapi.net/v3/articles?_limit=10';
 
 // middleware de nivel de aplicación
 app.use((req, res, next) => {
-  const cacheTime = 60*5; // 60 seconds * 5 minutes = 5 minutes
-  
+  const cacheTime = 60*5; // 60 seconds * 5 minutes = 5 minutes  
   if (req.method == 'GET') {
     res.set('Cache-control', `public, max-age=${cacheTime}`)
   } else {
-    // for the other requests set strict no caching parameters
     res.set('Cache-control', `no-store`)
   }
-
   next();
 });
 
-// title, url, imageUrl
+/**
+ * Metodo para obtener el listado y filtrarlo por los 3 campos requeridos
+ */
 app.get('/', async (req, res) => {
     try {
-        const { data } = await axios.get(__API__);
-        //return data;
-        let response = data.map(({title, url, imageUrl}) => {
-            return {title, url, imageUrl};
-        });
-
-        return res.json({data: response});
-
-      } catch (error) {
-        console.log(error);
+      const reply = await GET_ASYNC('articles');
+      if(reply) {
+          return res.json(JSON.parse(reply));
       }
+      
+      const { data } = await axios.get(__API__);
+      
+      let response = data.map(({title, url, imageUrl}) => {
+        return {title, url, imageUrl};
+      });
+      
+      await SET_ASYNC('articles', JSON.stringify({ data: response }));
+      return res.json({data: response});
+    } catch (error) {
+      console.log(error);
+    }
 })
  
 //Iniciando el servidor
